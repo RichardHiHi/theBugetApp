12ba2b63c3b722ee2443b614f0d6bb6ab6dbd0fb
thanh <thanhtrong.nguyen1602@gmail.com>
2020-09-22 19:13:58 +0700

    set eventlistener for buntton (add_btn)

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
+//buget controller
 var bugetcontroller = (function(){
+    return{
+        test : function(a) {
+            console.log(a)
+        }
+    }
+});
 
-})
 
+
+//UI controller
 var UIcontroller = (function(){
 
-})
+});
 
-var controller = (function(){
-    
-})
+
+//controller
+var controller = (function(budgetCtrl,UICtrl){
+
+    var ctrlAddItem = function(){
+         // get the input data
+        console.log("thanh")
+        // add add the item to the budget controller
+
+        // add the item to the UI
+
+        // calcular the budget 
+
+        // display the budget on the UI
+
+    }
+    document.querySelector('.add__btn').addEventListener('click',function () {
+       ctrlAddItem();
+    });
+
+    addEventListener('keypress', function(){
+        if(event.keyCode === 13 || event.which ===13){
+            ctrlAddItem();
+        }
+    })
+})(bugetcontroller,UIcontroller);
