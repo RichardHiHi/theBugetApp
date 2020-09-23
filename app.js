ac4978ae29c4d58f013d3ba5a62b5c8602ad570b
thanh <thanhtrong.nguyen1602@gmail.com>
2020-09-24 00:52:54 +0700

    get inputdata for UIcontroller

--- app.js
+++ app.js
@@ -1,28 +1,47 @@
 //buget controller
 var bugetcontroller = (function(){
-    return{
-        test : function(a) {
-            console.log(a)
-        }
-    }
-});
+  
+})();
 
 
 
 //UI controller
 var UIcontroller = (function(){
+     var DOMString ={
+        type : '.add__type',
+        description: '.add__description',
+        value : '.add__value',
+        btn : '.add__btn'
+        }  ;
+     
+    return{
+        // get input data 
+        getInput : function(){
+            return{
+                Type : document.querySelector(DOMString.type).value,
+                description : document.querySelector(DOMString.description).value,
+                Value : document.querySelector(DOMString.value).value
+            };        
+        },
 
-});
+        getDOMString : function(){
+            return DOMString;
+        }
+    };
+})();
 
 
 //controller
 var controller = (function(budgetCtrl,UICtrl){
+    
+    var getDOMString = UICtrl.getDOMString();
 
     var ctrlAddItem = function(){
          // get the input data
-        console.log("thanh")
-        // add add the item to the budget controller
-
+        var input = UICtrl.getInput();
+        console.log(input);
+        // add the item to the budget controller
+        
         // add the item to the UI
 
         // calcular the budget 
@@ -30,13 +49,13 @@ var controller = (function(budgetCtrl,UICtrl){
         // display the budget on the UI
 
     }
-    document.querySelector('.add__btn').addEventListener('click',function () {
-       ctrlAddItem();
-    });
+    
+    document.querySelector(getDOMString.btn).addEventListener('click',ctrlAddItem);
 
-    addEventListener('keypress', function(){
+    
+    addEventListener('keypress', function(event){
         if(event.keyCode === 13 || event.which ===13){
             ctrlAddItem();
         }
-    })
+    });
 })(bugetcontroller,UIcontroller);
